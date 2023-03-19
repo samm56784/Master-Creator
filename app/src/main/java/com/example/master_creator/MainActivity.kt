96019a199490490903a51f69597e73d4434b22f9
biolibi <biolibi@hotmail.com>
2023-03-19 17:34:46 -0400

    Voir fonction play() pour faire looper le nombre de chansons, actuellement peut seulement faire le setoncomletionlistener une fois, ensuite plus rien, voir a supprimer Musique.java par la suite ,car inutiliser

--- app/src/main/java/com/example/master_creator/MainActivity.kt
+++ app/src/main/java/com/example/master_creator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.master_creator
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -12,19 +13,72 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.master_creator.ui.theme.Master_CreatorTheme
 
+
+
 class MainActivity : ComponentActivity() {
+    lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContent {
             Master_CreatorTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting("Android")
+
+
+
                 }
             }
+
         }
+
+    }
+    override fun onResume() {
+        super.onResume()
+        play()
+
+
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+    private fun play(){
+
+        var index = 1;
+        val audioFile = "chanson"+index
+
+        val audioResourceId = resources.getIdentifier(audioFile, "raw", this.packageName)
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), audioResourceId);
+        mediaPlayer.start()
+
+        mediaPlayer.setOnCompletionListener(MediaPlayer.OnCompletionListener() {
+
+            mediaPlayer.release();
+            index += 1;
+
+            //ici on modifie
+            val audioFile = "chanson"+index
+            val audioResourceId = resources.getIdentifier(audioFile, "raw", packageName)
+
+
+            mediaPlayer = android.media.MediaPlayer.create(getApplicationContext(), audioResourceId);
+            mediaPlayer.start()
+        });
     }
 }
+
 
 
 
