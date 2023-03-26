9b8009f679f25142a34b1a393db5fba9763f882a
biolibi <biolibi@hotmail.com>
2023-03-26 13:32:40 -0400

    Branche deux, Oli, music + background image

--- app/src/main/java/com/example/master_creator/MainActivity.kt
+++ app/src/main/java/com/example/master_creator/MainActivity.kt
@@ -1,24 +1,31 @@
 package com.example.master_creator
 
 import android.media.MediaPlayer
+import android.media.MediaPlayer.OnCompletionListener
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.master_creator.ui.theme.Master_CreatorTheme
-
+import java.lang.reflect.Field
 
 
 class MainActivity : ComponentActivity() {
     lateinit var mediaPlayer: MediaPlayer
 
+    private val arrayListMusique = ArrayList<String>();
     override fun onCreate(savedInstanceState: Bundle?) {
+
 
         super.onCreate(savedInstanceState)
         setContent {
@@ -26,20 +33,24 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
                     Greeting("Android")
-
-
+                    DefaultPreview()
 
                 }
             }
-
         }
+
+        val fields : Array<Field> = R.raw::class.java.getFields();
+
+        for (i in 0..fields.size-1)
+            arrayListMusique.add(fields.get(i).name)
+
+        for (i in 0..fields.size-1)
+           System.out.println(arrayListMusique.get(i))
 
     }
     override fun onResume() {
         super.onResume()
         play()
-
-
 
     }
 
@@ -53,37 +64,33 @@ class MainActivity : ComponentActivity() {
         mediaPlayer.stop()
         mediaPlayer.release()
     }
-    private fun play(){
+    private var index = 0
 
-        var index = 1;
-        val audioFile = "chanson"+index
+    private fun play() {
+        var audioResourceId = resources.getIdentifier(arrayListMusique.get(index), "raw", packageName)
 
-        val audioResourceId = resources.getIdentifier(audioFile, "raw", this.packageName)
-
-        mediaPlayer = MediaPlayer.create(getApplicationContext(), audioResourceId);
+        mediaPlayer = MediaPlayer.create(this,audioResourceId)
         mediaPlayer.start()
 
-        mediaPlayer.setOnCompletionListener(MediaPlayer.OnCompletionListener() {
+        mediaPlayer.setOnCompletionListener{ mediaPlayer ->
+           index += 1
+            if (index == arrayListMusique.size) {
+                index = 0
+            }
+            mediaPlayer.release()
+            play()
 
-            mediaPlayer.release();
-            index += 1;
-
-            //ici on modifie
-            val audioFile = "chanson"+index
-            val audioResourceId = resources.getIdentifier(audioFile, "raw", packageName)
-
-
-            mediaPlayer = android.media.MediaPlayer.create(getApplicationContext(), audioResourceId);
-            mediaPlayer.start()
-        });
+        }
+       }
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 }
 
-
-
-
 @Composable
 fun Greeting(name: String) {
+
     Text(text = "Hello $name!")
 }
 fun MenuCreationMap(){
@@ -92,7 +99,13 @@ fun MenuCreationMap(){
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    Image(
+        painter = painterResource(id = R.drawable.menuprincipale),
+        contentDescription = null
+    )
+
     Master_CreatorTheme {
+
         Greeting("Android")
     }
 }
